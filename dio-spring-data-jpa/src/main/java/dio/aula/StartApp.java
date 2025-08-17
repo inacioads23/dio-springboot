d9ad02e1af760124caf788498f642efb08eaef58
inacioads23 <jinacio11@gmail.com>
2025-08-17 13:09:15 -0300

    Update applicattion.properties, StartApp.java

--- dio-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ dio-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -2,7 +2,6 @@ package dio.aula;
 
 import dio.aula.model.User;
 import dio.aula.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,24 +9,32 @@ import java.util.List;
 
 @Component
 public class StartApp implements CommandLineRunner {
-    @Autowired
-    private UserRepository repository;
-    @Override
-    public void run(String... args) throws Exception {
-        List<User> users = repository.findByNameContaining("GLEYSON");
-        for(User u: users){
-            System.out.println(u);
-        }
-    }
-    private void insertUser(){
-        User user = new User();
-        user.setName("GABRIEL NUNES");
-        user.setUsername("gabriel");
-        user.setPassword("santos");
-        repository.save(user);
+	// @Autowired
+	private UserRepository repository;
 
-        for(User u: repository.findAll()){
-            System.out.println(u);
-        }
-    }
+	public StartApp(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//insertUser(); // PARA INSERIR - Descomentar!!
+		//List<User> users = repository.findByNameContaining("GABRIEL");
+		List<User> users = repository.filtrarPorNome("GABRIEL");
+		for (User u : users) {
+			System.out.println(u);
+		}
+	}
+
+	private void insertUser() {
+		User user = new User();
+		user.setName("JOSE INACIO");
+		user.setUsername("jose");
+		user.setPassword("inacio");
+		repository.save(user);
+
+		for (User u : repository.findAll()) {
+			System.out.println(u);
+		}
+	}
 }
