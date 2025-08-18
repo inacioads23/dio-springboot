dc382906335a24a08bfe0e93b3df4e081d8c95c5
inacioads23 <jinacio11@gmail.com>
2025-08-18 11:04:58 -0300

    Update Swagger, pom.xml

---
+++ springboot-web-swagger/src/main/java/dio/springbootweb/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package dio.springbootweb;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Title - Rest API")
+                .description("API exemplo de uso de Springboot REST API")
+                .version("1.0")
+                .termsOfService("Termo de uso: Open Source")
+                .contact(new Contact()
+                    .name("Seu nome")
+                    .url("http://www.seusite.com.br")
+                    .email("voce@seusite.com.br"))
+                .license(new License()
+                    .name("Licença - Sua Empresa")
+                    .url("http://www.seusite.com.br")));
+    }
+}
--- springboot-web-swagger/src/main/java/dio/springbootweb/SwaggerConfigObsoleto.java
+++ springboot-web-swagger/src/main/java/dio/springbootweb/SwaggerConfigObsoleto.java
@@ -1,11 +1,6 @@
 package dio.springbootweb;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+/*
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class SwaggerConfig {
     private Contact contato() {
         return new Contact(
@@ -54,3 +49,4 @@ public class SwaggerConfig {
         return docket;
     }
 }
+*/
